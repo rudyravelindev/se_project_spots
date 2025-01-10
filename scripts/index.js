c9417d2ef76c28ce4af79c946eae6d063c1b1350
rudyravelindev <rudyravelindev@gmail.com>
2025-01-09 21:29:08 -0800

    adding the scripst with thw index.js file and add 7 objects

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Val Thorens",
+    link: "https://unsplash.com/fr/photos/foule-rassemblee-dans-le-parc-enneige-du-mont-Q1DWowDiki0",
+  },
+  {
+    name: "Restaurant terrace",
+    link: "https://unsplash.com/fr/photos/une-longue-table-en-bois-avec-des-assiettes-blanches-dessus-_HIUS4HSolw",
+  },
+  {
+    name: "An outdoor cafe",
+    link: "https://unsplash.com/fr/photos/un-couple-de-femmes-assises-a-une-table-devant-des-plantes-en-pot-VujVETzx6ms",
+  },
+  {
+    name: "A very long bridge, over the forest",
+    link: "https://unsplash.com/fr/photos/vue-aerienne-dune-route-entouree-darbres-verts-qzgN45hseN0",
+  },
+  {
+    name: "Tunnel with mornig light",
+    link: "https://unsplash.com/fr/photos/photo-a-faible-exposition-de-voitures-sur-la-route-pendant-la-nuit-MnHQMzC6n-o",
+  },
+  {
+    name: "Mountain house",
+    link: "https://unsplash.com/fr/photos/une-structure-en-bois-avec-une-lumiere-a-linterieur-6defJnqqIH8",
+  },
+];
