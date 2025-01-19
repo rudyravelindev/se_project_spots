facb622804bcd7f76cc0c379d414c4a64d8849cd
rudyravelindev <rudyravelindev@gmail.com>
2025-01-18 16:25:55 -0800

    Rendering cards done

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 const initialCards = [
   {
     name: "Val Thorens",
-    link: "https://unsplash.com/fr/photos/foule-rassemblee-dans-le-parc-enneige-du-mont-Q1DWowDiki0",
+    link: "https://images.unsplash.com/photo-1519659675643-e5885721661f?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Restaurant terrace",
-    link: "https://unsplash.com/fr/photos/une-longue-table-en-bois-avec-des-assiettes-blanches-dessus-_HIUS4HSolw",
+    link: "https://plus.unsplash.com/premium_photo-1723491285855-f1035c4c703c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "An outdoor cafe",
-    link: "https://unsplash.com/fr/photos/un-couple-de-femmes-assises-a-une-table-devant-des-plantes-en-pot-VujVETzx6ms",
+    link: "https://plus.unsplash.com/premium_photo-1664286774203-bb49682f41b1?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "A very long bridge, over the forest",
-    link: "https://unsplash.com/fr/photos/vue-aerienne-dune-route-entouree-darbres-verts-qzgN45hseN0",
+    link: "https://images.unsplash.com/photo-1476231682828-37e571bc172f?q=80&w=3474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Tunnel with mornig light",
-    link: "https://unsplash.com/fr/photos/photo-a-faible-exposition-de-voitures-sur-la-route-pendant-la-nuit-MnHQMzC6n-o",
+    link: "https://plus.unsplash.com/premium_photo-1673515243120-fd3691db5754?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     name: "Mountain house",
-    link: "https://unsplash.com/fr/photos/une-structure-en-bois-avec-une-lumiere-a-linterieur-6defJnqqIH8",
+    link: "https://images.unsplash.com/photo-1646070078388-4a566de7484d?q=80&w=2765&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
@@ -37,10 +37,32 @@ const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
 
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  // console.log(data);
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+  const cardNameElement = cardElement.querySelector(".card__title");
+  //To DO Select Img Element
+  const cardImageElement = cardElement.querySelector(".card__image");
+
+  // Assign the image src and alt attributes
+  cardImageElement.src = data.link;
+  cardImageElement.alt = data.name;
+
+  cardNameElement.textContent = data.name;
+
+  // Assign values to the img src and alt
+
+  return cardElement;
+}
+
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-
   editModal.classList.add("modal__opened");
 }
 
@@ -58,3 +80,8 @@ function handleEditFormSubmit(evt) {
 profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
