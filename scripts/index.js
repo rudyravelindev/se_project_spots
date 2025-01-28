acd9af55a34dde7d05b243850bdf31255370549e
rudyravelindev <rudyravelindev@gmail.com>
2025-01-27 19:21:10 -0800

    Rendering cards

--- scripts/index.js
+++ scripts/index.js
@@ -82,7 +82,7 @@ profileEditButton.addEventListener("click", openModal);
 editModalCloseBtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = getCardElement(initialCards[i]);
-  cardsList.prepend(cardElement);
-}
+initialCards.forEach((item) => {
+  const cardElement = getCardElement(item);
+  cardsList.append(cardElement);
+});
