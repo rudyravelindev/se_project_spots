d1568d407304b5a000f7fb3cdfb649b028ec73d0
rudyravelindev <rudyravelindev@gmail.com>
2025-02-11 21:18:01 -0800

    Part 3: enableValidation setEventListeners and showInputError done

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,1 +1,37 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgID = inputEl.id + "-error";
+  // const errorMsgEl = document.querySelector("#" + errorMsgID); I can use this too
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = errorMsg;
+};
 
+const checkInputValidity = (formEl, inputEl) => {
+  console.log(inputEl.validationMessage);
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  }
+};
+
+const setEventListeners = (formEl) => {
+  const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
+  const buttonElement = formEl.querySelector(".modal__submit-btn");
+
+  // TODO - handle initial states
+  // toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll("#modal__form, #add-card-form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  });
+};
+
+enableValidation();
